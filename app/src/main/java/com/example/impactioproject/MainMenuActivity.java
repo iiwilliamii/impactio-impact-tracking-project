814084452b556079aae8b41385980be0fa73cf65
wzhu5374 <williamzhu200300@gmail.com>
2022-07-12 01:59:29 +1000

    bug fixes and finishing RecyclerView

--- app/src/main/java/com/example/impactioproject/MainMenuActivity.java
+++ app/src/main/java/com/example/impactioproject/MainMenuActivity.java
@@ -1,16 +1,25 @@
 package com.example.impactioproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainMenuActivity extends AppCompatActivity {
     private TextView mName;
     private Button mProfile;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView mRecyclerView;
+    private ProjectAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,22 @@ public class MainMenuActivity extends AppCompatActivity {
 
         mName = findViewById(R.id.tv_name);
         mProfile = findViewById(R.id.btn_profile);
+        mRecyclerView = findViewById(R.id.rv_list);
+        mRecyclerView.setHasFixedSize(true);
 
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        ProjectAdapter.RecyclerViewClickListener listener = new ProjectAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, String projectSymbol) {
+                launchDetailActivity(projectSymbol);
+            }
+        };
+
+        mAdapter = new ProjectAdapter(Projects.getProjects(), listener);
+//        mAdapter.sort(ProjectAdapter.SORT_METHOD_NAME);
+        mRecyclerView.setAdapter(mAdapter);
         mProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,8 +51,16 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
     }
+
+
     private void launchProfileActivity() {
         Intent intent = new Intent(MainMenuActivity.this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    private void launchDetailActivity(String projectSymbol) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("Symbol", projectSymbol);
         startActivity(intent);
     }
 }
