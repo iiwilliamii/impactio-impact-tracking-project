03997c3e50c07079c6e93a53b6abe0d19cdd80ba
wzhu5374 <williamzhu200300@gmail.com>
2022-07-12 00:24:19 +1000

    Minor Bug fixes and linking pages together

--- app/src/main/java/com/example/impactioproject/MainMenuActivity.java
+++ app/src/main/java/com/example/impactioproject/MainMenuActivity.java
@@ -2,13 +2,33 @@ package com.example.impactioproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainMenuActivity extends AppCompatActivity {
+    private TextView mName;
+    private Button mProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        mName = findViewById(R.id.tv_name);
+        mProfile = findViewById(R.id.btn_profile);
+
+        mProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchProfileActivity();
+            }
+        });
+    }
+    private void launchProfileActivity() {
+        Intent intent = new Intent(MainMenuActivity.this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
