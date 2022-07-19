be9ee1b6d0c17759dbc45ae8cf9125f4901a5eab
nghugo88 <nghugo88@gmail.com>
2022-07-19 15:11:36 +1000

    Minor updates to the login page

--- app/src/main/java/com/example/impactioproject/activities/PrimaryLoginActivity.java
+++ app/src/main/java/com/example/impactioproject/activities/PrimaryLoginActivity.java
@@ -1,16 +1,42 @@
 package com.example.impactioproject.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.impactioproject.R;
+import com.example.impactioproject.projects.ProjectAdapter;
+import com.example.impactioproject.projects.Projects;
 
 public class PrimaryLoginActivity extends AppCompatActivity {
+    private TextView mName;
+    private Button mLogin;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView mRecyclerView;
+    private ProjectAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primary_login);
+
+        mLogin = findViewById(R.id.loginBtn);
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchLoginActivity();
+            }
+        });
+    }
+
+    private void launchLoginActivity() {
+        Intent intent = new Intent(this, LoginActivityMain.class);
+        startActivity(intent);
     }
 }
