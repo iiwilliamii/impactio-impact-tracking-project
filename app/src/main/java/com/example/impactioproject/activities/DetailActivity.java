918aad9f1c1fc16b28057fefe52807d135ee4d24
wzhu5374 <williamzhu200300@gmail.com>
2022-07-21 00:19:34 +1000

    project activity update setup

--- app/src/main/java/com/example/impactioproject/activities/DetailActivity.java
+++ app/src/main/java/com/example/impactioproject/activities/DetailActivity.java
@@ -2,15 +2,34 @@ package com.example.impactioproject.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.impactioproject.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private Button mBtnActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mBtnActivity = findViewById(R.id.btn_activity);
+        mBtnActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchProjectUpdates();
+            }
+        });
+    }
+
+    private void launchProjectUpdates() {
+        Intent intent = new Intent(this, ProjectUpdateActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/impactioproject/activities/ProjectUpdateActivity.java
@@ -0,0 +1,16 @@
+package com.example.impactioproject.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.impactioproject.R;
+
+public class ProjectUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_update);
+    }
+}
