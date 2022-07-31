a4067f7b11cef60beae84165cce111e2db146f85
nghugo88 <nghugo88@gmail.com>
2022-08-01 00:34:08 +1000

    Impactio Impact Tracking MVP Application completed.

--- app/src/main/java/com/example/impactioproject/activities/TrackerAddActivity.java
+++ app/src/main/java/com/example/impactioproject/activities/TrackerAddActivity.java
@@ -30,17 +30,22 @@ public class TrackerAddActivity extends AppCompatActivity {
     mNameInput = findViewById(R.id.et_nameInput);
     sdgInput = findViewById(R.id.sdg_add);
     mdescInput = findViewById(R.id.et_description);
-}
 
+}
     public void addtracker(View view) {
-        String name = mNameInput.getText().toString();
-        String sdg = sdgInput.getText().toString();
+        score += 50;
+        String c4title = "Custom Criteria #1";
+        c4name = mNameInput.getText().toString();
+        c4sdg = sdgInput.getText().toString();
+        c4description = mdescInput.getText().toString();
         SharedPreferences sp2 = getSharedPreferences("TrackerName", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp2.edit();
-        editor.putString("name", name);
-        editor.putString("sdg", sdg);
+        editor.putString("c4title", c4title);
+        editor.putString("c4name", c4name);
+        editor.putString("c4sdg", c4sdg);
+        editor.putInt("score", score);
         editor.apply();
-        String loginSuccess = "Tracker added!";
+        String loginSuccess = "Custom Criteria added successfully!";
         Toast.makeText(TrackerAddActivity.this, loginSuccess, Toast.LENGTH_SHORT).show();
 
         Intent intent = new Intent(TrackerAddActivity.this, DetailActivity.class);
